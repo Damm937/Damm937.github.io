2db8db8cd63a7c2497ff236e6211a160f4c53393
Damm937 <114189748+Damm937@users.noreply.github.com>
2023-03-08 12:09:53 -0300

    Add files via upload

---
+++ Ticmas/JS/asd.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Empezo todo en la escuela secundaria");
+    document.getElementById("boton").innerHTML = "Tecnico en Programacion Web en la UNLaM"
+}
